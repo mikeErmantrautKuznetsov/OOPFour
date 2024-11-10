24095456d425bc3cf04d3269e162cd0c92831f0b
mikeErmantrautKuznetsov <128633238+mikeErmantrautKuznetsov@users.noreply.github.com>
2024-11-10 21:59:26 +0300

    Four

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace OOPFourth
+{
+    //Колода карт
+    //Есть колода с картами.Игрок достает карты, пока не решит, что ему хватит карт
+    //(может быть как выбор пользователя, так и количество сколько карт надо взять).
+    //После выводиться вся информация о вытянутых картах.
+    //Возможные классы: Карта, Колода, Игрок.
+
+    public class Player
+    {
+        Player player = new Player();
+    }
+
+    public class Card
+    {
+        public List<int> cardDelet = new List<int>();
+    }
+
+    public class Deck
+    {
+        public List<int> deckAdd = new List<int>() { 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck.deckAdd.Add(1);
+            deck.deckAdd.Add(2);
+            for (int i = 0; i < deck.deckAdd.Count; i++)
+            {
+                deck.deckAdd[i] = i;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
